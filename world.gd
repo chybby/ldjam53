a1382cd64b36d39967f7fb2b6f5975a0c0a04bc8
Adam Chyb <achybby@gmail.com>
2023-04-30 01:49:49 +1000

    Changed level layout and some lighting.

--- world.gd
+++ world.gd
@@ -3,9 +3,16 @@ extends Node3D
 var PackageScene = preload("res://package/package.tscn")
 
 @onready var level := $Level
+@onready var package_spawn_timer := $PackageSpawnTimer
+
+var packages_left = 10
 
 func _on_package_spawn_timer_timeout():
     var package := PackageScene.instantiate()
     package.randomize()
     package.position = level.get_package_spawn_position()
     add_child(package)
+
+    packages_left -= 1
+    if packages_left == 0:
+        package_spawn_timer.stop()
