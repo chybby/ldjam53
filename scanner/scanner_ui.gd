7c518ac9e87a2cb031b8ef46b21958e44c0f737d
Adam Chyb <achybby@gmail.com>
2023-05-01 02:23:12 +1000

    Nicer scanner.

--- scanner/scanner_ui.gd
+++ scanner/scanner_ui.gd
@@ -1,10 +1,17 @@
 extends Control
 
+@onready var scan_something_label := $Panel/ScanSomethingLabel
 @onready var name_label := $Panel/NameLabel
 @onready var address_label := $Panel/AddressLabel
 
-func set_name_label(name: String):
+func display_name_address(name: String, address: String):
+    name_label.visible = true
     name_label.text = name
-
-func set_address_label(address: String):
+    address_label.visible = true
     address_label.text = address
+    scan_something_label.visible = false
+
+func clear_display():
+    name_label.visible = false
+    address_label.visible = false
+    scan_something_label.visible = true
